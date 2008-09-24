4a7fa37577bf9673d74a4cf1c6a2a9f317d4294b
eberry <eberry@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2008-09-24 23:13:21 +0000

    Update Delphi 8 paths
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@280 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- BuildInstall/CompileDelphi8.bat
+++ BuildInstall/CompileDelphi8.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 SET SRCDIR=D:\Projects\GExperts\Source\
-SET SYNEDIT=D:\Borland\SynEdit\Source
+SET SYNEDIT=D:\Projects\GExperts\ExternalSource\UniSynEdit
 SET DELPHIDIR=D:\Borland\Delphi7\d8bin
-SET SWITCHES=-B -DGX_AutomatedBuild -DSYNEDIT -LUdesignide -LUvcl -LUvclx -LUrtl -$D- -$L- -$Q+ -$R+ -$T+ -I..\Source\Framework -UD:\Borland\Delphi7\d8lib -U..\ExternalSource -U..\ExternalSource\Abbrevia -U%SYNEDIT%
+SET SWITCHES=-B -DGX_AutomatedBuild -DSYNEDIT -LUdesignide -LUvcl -LUvclx -LUrtl -LUvcljpg -$D- -$L- -$Q+ -$R+ -$T+ -I..\Source\Framework -UD:\Borland\Delphi7\d8lib -U..\ExternalSource -U..\ExternalSource\Abbrevia -U%SYNEDIT%
 CD %SRCDIR%
 
 %DELPHIDIR%\dcc32.exe %SWITCHES% GExpertsD8.dpr
