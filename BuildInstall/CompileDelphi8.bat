8ecabdb0b4b336fb60cf3a952b218083bd95eb27
eberry <eberry@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2010-12-28 14:45:50 +0000

    Get all IDEs enabled again Delphi 8 compilation tweaks
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@594 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- BuildInstall/CompileDelphi8.bat
+++ BuildInstall/CompileDelphi8.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 SET SRCDIR=D:\Projects\GExperts\Source\
 SET SYNEDIT=D:\Projects\GExperts\ExternalSource\UniSynEdit
-SET DELPHIDIR=D:\Borland\Delphi7\d8bin
-SET SWITCHES=-B -DGX_AutomatedBuild -DSYNEDIT -LUdesignide -LUvcl -LUvclx -LUrtl -LUvcljpg -$D- -$L- -$Q+ -$R+ -$T+ -I..\Source\Framework -UD:\Borland\Delphi7\d8lib -U..\ExternalSource -U..\ExternalSource\Abbrevia -U%SYNEDIT%
+SET DELPHIDIR=D:\IDE\Delphi 7\d8bin
+SET SWITCHES=-B -DGX_AutomatedBuild -DSYNEDIT -LUdesignide -LUvcl -LUvclx -LUrtl -LUvcljpg -$D- -$L- -$Q+ -$R+ -$T+ -I..\Source\Framework -U"D:\IDE\Delphi 7\d8lib" -U..\ExternalSource -U..\ExternalSource\Abbrevia -U%SYNEDIT%
 CD %SRCDIR%
 
-%DELPHIDIR%\dcc32.exe %SWITCHES% GExpertsD8.dpr
+"%DELPHIDIR%\dcc32.exe" %SWITCHES% GExpertsD8.dpr
