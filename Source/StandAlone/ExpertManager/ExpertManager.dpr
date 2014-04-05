9b10876bc64d291293e4d414d99696987ae9bd0d
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2014-04-05 13:44:00 +0000

    Now allows one command line parameter: The DLL to load. This way it can be used to register different GExperts versions without having to be compiled with the corresponding Delphi version.
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@797 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- Source/StandAlone/ExpertManager/ExpertManager.dpr
+++ Source/StandAlone/ExpertManager/ExpertManager.dpr
@@ -3,10 +3,29 @@ program ExpertManager;
 {$R *.res}
 
 uses
+  Windows,
+  SysUtils,
   GX_VerDepConst in '..\..\Framework\GX_VerDepConst.pas';
 
-procedure ShowExpertManager; external GExpertsDll;
+type
+  TShowExpertManager = procedure;
+
+var
+  GExpertsDLLName: string;
+  Handle: HMODULE;
+  ShowExpertManager: TShowExpertManager;
 begin
+  if ParamCount > 0 then
+    GExpertsDLLName := ParamStr(1)
+  else
+    GExpertsDLLName := GExpertsDll;
+
+  Handle := SysUtils.SafeLoadLibrary(GExpertsDLLName);
+  if Handle = 0 then
+    raise Exception.CreateFmt('Could not load library %s.', [GExpertsDLLName]);
+  @ShowExpertManager := GetProcAddress(Handle, 'ShowExpertManager');
+  if not Assigned(ShowExpertManager) then
+    raise Exception.CreateFmt('Could not find entry point "ShowExpertManager" in dll %s', [GExpertsDLLName]);
   ShowExpertManager;
 end.
 
