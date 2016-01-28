a08e91ced8f70fb3a403eb17825e65b4f7fb92b4
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2016-01-28 20:12:30 +0000

    new hook for TMessageEvent (TApplication.OnMessage)
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@1290 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- Source/Framework/GX_EventHook.pas
+++ Source/Framework/GX_EventHook.pas
@@ -3,8 +3,10 @@ unit GX_EventHook;
 interface
 
 uses
+  Windows,
   SysUtils,
-  Classes;
+  Classes,
+  Forms;
 
 type
   ///<summary>
@@ -27,16 +29,37 @@ type
     constructor Create(_OrigEvent: TMethod; _HookEvent: TMethod);
   end;
 
+type
+  ///<summary>
+  /// This class is meant to be used as the destination of a TMessageEventHook.
+  /// It is kept very simple on purpose. Use it as is, do *not* add to it or derive from it, do
+  /// *not* rename the class.
+  /// See the procedure below on how to use it.
+  TMessageEventHook = class
+  public
+    ///<summary>
+    /// Store the original event here. </summary>
+    OrigEvent: TMethod;
+    ///<summary>
+    /// Store the event you want to call here. </summary>
+    HookEvent: TMethod;
+    ///<summary>
+    /// Assign this method to the Event you want to hook.
+    /// It first calls HookEvent and afterwards OrigEvent. </summary>
+    procedure HandleEvent(var _Msg: TMsg; var _Handled: Boolean); virtual;
+    constructor Create(_OrigEvent: TMethod; _HookEvent: TMethod);
+  end;
+
 function HookScreenActiveFormChange(_HookEvent: TNotifyEvent): TNotifyEventHook;
 procedure UnhookScreenActiveFormChange(_Hook: TNotifyEventHook);
 
 function HookScreenActiveControlChange(_HookEvent: TNotifyEvent): TNotifyEventHook;
 procedure UnhookScreenActiveControlChange(_Hook: TNotifyEventHook);
 
-implementation
+function HookApplicationOnMessage(_HookEvent: TMessageEvent): TMessageEventHook;
+procedure UnhookApplicationOnMessage(_Hook: TMessageEventHook);
 
-uses
-  Forms;
+implementation
 
 { TNotifyEventHook }
 
@@ -55,6 +78,23 @@ begin
     TNotifyEvent(OrigEvent)(_Sender);
 end;
 
+{ TMessageEventHook }
+
+constructor TMessageEventHook.Create(_OrigEvent, _HookEvent: TMethod);
+begin
+  inherited Create;
+  OrigEvent := _OrigEvent;
+  HookEvent := _HookEvent;
+end;
+
+procedure TMessageEventHook.HandleEvent(var _Msg: TMsg; var _Handled: Boolean);
+begin
+  if Assigned(HookEvent.Data) and Assigned(HookEvent.Code) then
+    TMessageEvent(HookEvent)(_Msg, _Handled);
+  if Assigned(OrigEvent.Data) and Assigned(OrigEvent.Code) then
+    TMessageEvent(OrigEvent)(_Msg, _Handled);
+end;
+
 /// Assume you want to hook Screen.OnActiveFormChange (which is a TNotifyEvent) with
 /// the method HandleActiveFormChange of your already existing object MyObject.
 /// So you call HookScreenActiveFormChange like this:
@@ -70,11 +110,11 @@ procedure UnhookScreenActiveFormChange(_Hook: TNotifyEventHook);
 var
   Ptr: TMethod;
 begin
- if not Assigned(_Hook) then begin
+  if not Assigned(_Hook) then begin
    // Just in case somebody did not check whether HookScreenActiveFormChange actually returned
    // a valid object or simply didn't call it.
-   Exit;
- end;
+    Exit;
+  end;
 
   Ptr := TMethod(Screen.OnActiveFormChange);
   if not Assigned(Ptr.Data) and not Assigned(Ptr.Code) then begin
@@ -103,7 +143,7 @@ begin
 
   // If we get here, somebody who does not adhere to this standard has changed the event.
   // The best thing we can do, is Assign NIL to the HookEvent so it no longer gets called.
-  // We cannot free the hook because there might the somebody might still have reference
+  // We cannot free the hook because somebody might still have reference
   // to _Hook.HandleEvent.
   _Hook.HookEvent.Code := nil;
   _Hook.HookEvent.Data := nil;
@@ -124,11 +164,11 @@ procedure UnhookScreenActiveControlChange(_Hook: TNotifyEventHook);
 var
   Ptr: TMethod;
 begin
- if not Assigned(_Hook) then begin
-   // Just in case somebody did not check whether HookScreenActiveFormChange actually returned
+  if not Assigned(_Hook) then begin
+   // Just in case somebody did not check whether UnhookScreenActiveControlChange actually returned
    // a valid object or simply didn't call it.
-   Exit;
- end;
+    Exit;
+  end;
 
   Ptr := TMethod(Screen.OnActiveControlChange);
   if not Assigned(Ptr.Data) and not Assigned(Ptr.Code) then begin
@@ -157,14 +197,59 @@ begin
 
   // If we get here, somebody who does not adhere to this standard has changed the event.
   // The best thing we can do, is Assign NIL to the HookEvent so it no longer gets called.
-  // We cannot free the hook because there might the somebody might still have reference
+  // We cannot free the hook because somebody might still have reference
+  // to _Hook.HandleEvent.
+  _Hook.HookEvent.Code := nil;
+  _Hook.HookEvent.Data := nil;
+end;
+
+function HookApplicationOnMessage(_HookEvent: TMessageEvent): TMessageEventHook;
+begin
+  Result := TMessageEventHook.Create(TMethod(Application.OnMessage), TMethod(_HookEvent));
+  Application.OnMessage := Result.HandleEvent;
+end;
+
+procedure UnhookApplicationOnMessage(_Hook: TMessageEventHook);
+var
+  Ptr: TMethod;
+begin
+  if not Assigned(_Hook) then begin
+   // Just in case somebody did not check whether HookApplicationOnMessage actually returned
+   // a valid object or simply didn't call it.
+    Exit;
+  end;
+
+  Ptr := TMethod(Application.OnMessage);
+  if not Assigned(Ptr.Data) and not Assigned(Ptr.Code) then begin
+    // Somebody has assigned NIL to the event.
+    // It's probably safe to assume that there will be no reference to our hook left, so we just
+    // free the object and be done.
+    _Hook.Free;
+    Exit;
+  end;
+
+  while TObject(Ptr.Data).ClassNameIs('TMessageEventHook') do begin
+    // Somebody who knows about this standard has hooked the event.
+    // (Remember: Do not change the class name or the class structure. Otherwise this
+    //  check will fail!)
+    // Let's check whether we can find our own hook in the chain.
+    if Ptr.Data = _Hook then begin
+      // We are lucky, nobody has tampered with the event, we can just assign the original event,
+      // free the hook object and be done with it.
+      Application.OnMessage := TMessageEvent(_Hook.OrigEvent);
+      _Hook.Free;
+      Exit;
+    end;
+    // check the next event in the chain
+    Ptr := TMethod(TMessageEventHook(Ptr.Data).OrigEvent);
+  end;
+
+  // If we get here, somebody who does not adhere to this standard has changed the event.
+  // The best thing we can do, is Assign NIL to the HookEvent so it no longer gets called.
+  // We cannot free the hook because somebody might still have reference
   // to _Hook.HandleEvent.
   _Hook.HookEvent.Code := nil;
   _Hook.HookEvent.Data := nil;
 end;
 
 end.
-
-
-
-
