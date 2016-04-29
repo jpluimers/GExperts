eef3a90fc6a15e5bdd37f67861eff54ab4b3b6d6
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2016-04-29 20:40:22 +0000

    * now loads the newest available GExperts DLL * upgraded to Delphi 2007 * deleted unused .dof files * added open and compile script
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@1485 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- Source/StandAlone/Grep/GExpertsGrep.dpr
+++ Source/StandAlone/Grep/GExpertsGrep.dpr
@@ -3,10 +3,62 @@ program GExpertsGrep;
 {$R *.res}
 
 uses
+  Windows,
+  SysUtils,
+  Classes,
   GX_VerDepConst in '..\..\Framework\GX_VerDepConst.pas';
 
-procedure ShowGrep; external GExpertsDll;
+procedure LoadDll(const _GExpertsDLLName: string);
+type
+  TShowGrep = procedure;
+var
+  Handle: Cardinal;
+  ShowGrep: TShowGrep;
+begin
+  Handle := SysUtils.SafeLoadLibrary(_GExpertsDLLName);
+  if Handle = 0 then
+    raise Exception.CreateFmt('Could not load library %s.', [_GExpertsDLLName]);
+  @ShowGrep := GetProcAddress(Handle, 'ShowGrep');
+  if not Assigned(ShowGrep) then
+    raise Exception.CreateFmt('Could not find entry point "ShowGrep" in dll %s', [_GExpertsDLLName]);
+  ShowGrep;
+end;
+
+procedure Main;
+var
+  sl: TStringList;
+  i: Integer;
+begin
+  sl := TStringList.Create;
+  try
+    sl.Add('GExpertsD6.dll');
+    sl.Add('GExpertsD7.dll');
+    sl.Add('GExpertsD8.dll');
+    sl.Add('GExpertsDelphi2005.dll');
+    sl.Add('GExpertsBDS2006.dll');
+    sl.Add('GExpertsDelphi2007.dll');
+    sl.Add('GExpertsRS2009.dll');
+    sl.Add('GExpertsRS2010.dll');
+    sl.Add('GExpertsRSXE1.dll');
+    sl.Add('GExpertsRSXE2.dll');
+    sl.Add('GExpertsRSXE3.dll');
+    sl.Add('GExpertsRSXE4.dll');
+    sl.Add('GExpertsRSXE5.dll');
+    sl.Add('GExpertsRSXE6.dll');
+    sl.Add('GExpertsRSXE7.dll');
+    sl.Add('GExpertsRSXE8.dll');
+    sl.Add('GExpertsRS10.dll');
+    sl.Add('GExpertsRS101.dll');
+    for i := sl.Count - 1 downto 0 do
+      if FileExists(sl[i]) then begin
+        LoadDll(sl[i]);
+        Exit;
+      end;
+  finally
+    FreeAndNil(sl);
+  end;
+end;
 
 begin
-  ShowGrep;
+  Main;
 end.
