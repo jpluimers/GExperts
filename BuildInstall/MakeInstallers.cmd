bebbd87ee913c4bbdea3a51b7794d6f11b38f325
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2016-05-07 10:34:25 +0000

    now determines the date automatically
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@1505 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- BuildInstall/MakeInstallers.cmd
+++ BuildInstall/MakeInstallers.cmd
@@ -1,9 +1,9 @@
 @echo off
 set iscc="C:\Program Files (x86)\Inno Setup 5\ISCC.exe"
 set version=1.38
-set date=2016-05-05
 set instver=138
-set special=-experimental-twm-%date%
+call :getdate
+set special=-experimental-twm-%dateYYYYMMDD%
 
 call :makeinst Delphi6 D6
 call :makeinst Delphi7 D7
@@ -33,4 +33,35 @@ if "%prefix%" == "" set Prefix=%Delphi%
 rem %iscc% GExperts.iss /dDelphi2010 /dVersion=%version% /fGXRS2010-%instver%-experimental-twm-%date%.exe
 goto :eof
 
+:getdate
+rem taken from http://stackoverflow.com/a/16349176/49925
+:: Check WMIC is available
+WMIC.EXE Alias /? >NUL 2>&1 || GOTO s_error
 
+:: Use WMIC to retrieve date and time
+FOR /F "skip=1 tokens=1-6" %%G IN ('WMIC Path Win32_LocalTime Get Day^,Hour^,Minute^,Month^,Second^,Year /Format:table') DO (
+   IF "%%~L"=="" goto s_done
+      Set _yyyy=%%L
+      Set _mm=00%%J
+      Set _dd=00%%G
+      Set _hour=00%%H
+      SET _minute=00%%I
+      SET _second=00%%K
+)
+:s_done
+
+:: Pad digits with leading zeros
+      Set _mm=%_mm:~-2%
+      Set _dd=%_dd:~-2%
+      Set _hour=%_hour:~-2%
+      Set _minute=%_minute:~-2%
+      Set _second=%_second:~-2%
+
+Set dateYYYYMMDD=%_yyyy%-%_mm%-%_dd%
+set timeHHMMSS=hour%_%_minute%_%_second%
+Set logtimestamp=%dateYYYYMMDD%_%timeHHMMSS%
+goto :eof
+
+:s_error
+echo WMIC is not available, cannot deterimine date and time
+pause
