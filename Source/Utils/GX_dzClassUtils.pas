f2e559c218971eb98be488e0acb42c716df9323a
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2016-06-19 13:58:12 +0000

    new helper procedures to handle TStrings / TStringList
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@1630 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- Source/Utils/GX_dzClassUtils.pas
+++ Source/Utils/GX_dzClassUtils.pas
@@ -8,18 +8,68 @@ interface
 
 uses
   SysUtils,
-  Classes;
+  Classes,
+  GX_GenericUtils;
+
+///<summary>
+/// Assigns st to sl and sorts it.
+/// sl.Objects contains the index into st+1
+///            so st[Integer(ls.Objects[i])] = sl[i] </summary>
+procedure TStrings_GetAsSortedList(_st: TStrings; _sl: TStringList; _Duplicates: TDuplicates = dupAccept);
+
+///<summary>
+/// assign the current index to the Objects property and then sort the list </summary>
+procedure TStringList_MakeIndex(_sl: TStringList);
+
+procedure TGXUnicodeStringList_MakeIndex(_sl: TGXUnicodeStringList);
 
 function TStrings_ValueFromIndex(_st: TStrings; _Idx: integer): string;
 
 implementation
 
+procedure TStrings_GetAsSortedList(_st: TStrings; _sl: TStringList; _Duplicates: TDuplicates = dupAccept);
+begin
+  Assert(Assigned(_st));
+  Assert(Assigned(_sl));
+
+  _sl.Sorted := False;
+  _sl.Assign(_st);
+  TStringList_MakeIndex(_sl);
+end;
+
 function TStrings_ValueFromIndex(_st: TStrings; _Idx: integer): string;
 var
   Name: string;
 begin
+  Assert(Assigned(_st));
+
   Name := _st.Names[_Idx];
   Result := _st.Values[Name];
 end;
 
-end. 
+procedure TStringList_MakeIndex(_sl: TStringList);
+var
+  i: integer;
+begin
+  Assert(Assigned(_sl));
+
+  _sl.Sorted := False;
+  for i := 0 to _sl.Count - 1 do
+    _sl.Objects[i] := POinter(i + 1);
+  _sl.Sorted := true;
+end;
+
+procedure TGXUnicodeStringList_MakeIndex(_sl: TGXUnicodeStringList);
+var
+  i: integer;
+begin
+  Assert(Assigned(_sl));
+
+  _sl.Sorted := False;
+  for i := 0 to _sl.Count - 1 do
+    _sl.Objects[i] := POinter(i + 1);
+  _sl.Sorted := true;
+end;
+
+end.
+
