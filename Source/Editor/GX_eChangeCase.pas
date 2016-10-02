c5697318f2e5db6c16110cc088364cf585006717
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2016-10-02 11:12:15 +0000

    Replaced the dialog with a popup menu, the default entry is executed by pressing enter without first selecting any entry.
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@1789 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- Source/Editor/GX_eChangeCase.pas
+++ Source/Editor/GX_eChangeCase.pas
@@ -2,44 +2,97 @@ unit GX_eChangeCase;
 
 interface
 
-uses
-  Classes, Controls, StdCtrls, ExtCtrls, Forms, GX_BaseForm;
-
-type
-  TfmChangeCase = class(TfmBaseForm)
-    grpCaseSelection: TRadioGroup;
-    btnOK: TButton;
-    btnCancel: TButton;
-  end;
-
 implementation
 
-uses SysUtils, GX_EditorExpert, GX_GenericUtils, GX_eSelectionEditorExpert;
-
-{$R *.dfm}
+uses
+  SysUtils, Classes, Windows, Menus, Forms, Controls, ToolsAPI,
+  GX_EditorExpert, GX_GenericUtils, GX_OtaUtils,
+  {$IFOPT D+} GX_DbugIntf, {$ENDIF}
+  GX_dzVclUtils;
 
 type
-  TChangeCaseExpert = class(TSelectionEditorExpert)
+  TChangeCase = (ccLowerCase, ccUpperCase, ccTitleCase, ccSentenceCase, ccToggleCase, ccNone);
+
+type
+  TChangeCaseExpert = class(TEditorExpert)
   private
     FLastSelection: Integer;
+    FPopup: TPopupMenu;
+    FCodeList: TStringList;
+    FKBHook: HHOOK;
+    function IsEditorActive(out ctl: TWinControl): Boolean;
+    procedure miChangeToLowerCaseClicked(Sender: TObject);
+    procedure miChangeToUpperCaseClicked(Sender: TObject);
+    procedure miChangeToTitleCaseClicked(Sender: TObject);
+    procedure miChangeToSentenceCaseClicked(Sender: TObject);
+    procedure miToggleCaseClicked(Sender: TObject);
+    procedure ReplaceSelection;
+    function BlockSelectionToLineEndType(SourceEditor: IOTASourceEditor;
+      const Selection: string): Integer;
   protected
     function GetDisplayName: string; override;
     class function GetName: string; override;
-    function ProcessSelected(Lines: TStrings): Boolean; override;
-    function GetNoSelectionMode: TNoSelectionMode; override;
   public
     constructor Create; override;
+    destructor Destroy; override;
+    procedure Execute(Sender: TObject); override;
     function GetDefaultShortCut: TShortCut; override;
     function GetHelpString: string; override;
     function HasConfigOptions: Boolean; override;
   end;
 
+var
+  EnterWasPressed: Boolean = False;
+
+// This keyboard hook is necessary because I found no other way to detect if the
+// user closed the popup menu by pressing the enter key or by some other means.
+// When pressing enter, the default menu item is executed, otherwise nothing.
+// So this hook checks whether the enter key is pressed and sets a global flag,
+// which is then checked in TChangeCaseExpert.Execute.
+function KeyboardHookProc(Code: Integer; WordParam: Word; LongParam: LongInt) : LongInt;  stdcall;
+begin
+  if WordParam = VK_RETURN then
+    EnterWasPressed := True;
+  // let Windows pass on the key
+  Result := 0;
+end;
+
 { TChangeCaseExpert }
 
 constructor TChangeCaseExpert.Create;
+resourcestring
+  SChangeToLowerCase = '&lowercase';
+  SChangeToUpperCase = '&UPPER CASE';
+  SChangeToTitleCase = '&Title Case';
+  SChangeToSentenceCase = '&Sentence case';
+  SToggleCase = 't&OGGLE cASE';
+var
+  mi: TMenuItem;
 begin
   inherited;
   FLastSelection := 0;
+  FPopup := TPopupMenu.Create(nil);
+  mi := TPopupMenu_AppendMenuItem(FPopup, SChangeToLowerCase, miChangeToLowerCaseClicked);
+  mi.Tag := Ord(ccLowerCase);
+  mi := TPopupMenu_AppendMenuItem(FPopup, SChangeToUpperCase, miChangeToUpperCaseClicked);
+  mi.Tag := Ord(ccUpperCase);
+  mi := TPopupMenu_AppendMenuItem(FPopup, SChangeToTitleCase, miChangeToTitleCaseClicked);
+  mi.Tag := Ord(ccTitleCase);
+  mi := TPopupMenu_AppendMenuItem(FPopup, SChangeToSentenceCase, miChangeToSentenceCaseClicked);
+  mi.Tag := Ord(ccSentenceCase);
+  mi := TPopupMenu_AppendMenuItem(FPopup, SToggleCase, miToggleCaseClicked);
+  mi.Tag := Ord(ccToggleCase);
+  FCodeList := TStringList.Create;
+
+  FKBHook := SetWindowsHookEx(WH_KEYBOARD, @KeyboardHookProc, HInstance, GetCurrentThreadId());
+end;
+
+destructor TChangeCaseExpert.Destroy;
+begin
+  UnHookWindowsHookEx(FKBHook);
+  FreeAndNil(FCodeList);
+  FreeAndNil(FPopup);
+  inherited;
 end;
 
 function TChangeCaseExpert.GetDefaultShortCut: TShortCut;
@@ -68,9 +121,58 @@ begin
   Result := 'ChangeCase';
 end;
 
-function TChangeCaseExpert.GetNoSelectionMode: TNoSelectionMode;
+// WARNING: This is a hack.
+// GxOtaReplaceSelection has a problem with appending an
+// unwanted CRLF for multiline selections to the line end
+// The clean solution is just to call
+//    GxOtaReplaceSelection(Editor, 0, CodeList.Text);
+// and have the problem solved elsewhere, but this is hard...
+// We don't remove the CRLF if the block ends in the first column,
+// because then we delete a necessary CRLF.
+// todo: This is copied from TSelectionEditorExpert, maybe this
+// should go to TEditorExpert or to some intermediate class.
+function TChangeCaseExpert.BlockSelectionToLineEndType(SourceEditor: IOTASourceEditor;
+  const Selection: string): Integer;
+var
+  View: IOTAEditView;
+  CharPos: TOTACharPos;
+  EditPos: TOTAEditPos;
 begin
-  Result := nsmSelectIdent;
+  Result := 0;
+  View := GxOtaGetTopMostEditView(SourceEditor);
+  Assert(Assigned(View));
+  CharPos.Line := View.Block.EndingRow;
+  CharPos.CharIndex := View.Block.EndingColumn;
+  View.ConvertPos(False, EditPos, CharPos);
+  // TODO 3 -cIssue -oAnyone: Why is a 2 necessary for this column check (tested in Delphi 6)
+  if (Length(Selection) > 1) and (EditPos.Col > 2) then
+  begin
+    if Copy(Selection, Length(Selection) - 1, 2) = #13#10 then
+      Result := 2
+    else if Selection[Length(Selection)] = #10 then
+      Result := 1;
+  end;
+end;
+
+procedure TChangeCaseExpert.ReplaceSelection;
+var
+  SourceEditor: IOTASourceEditor;
+  LineEndType: Integer;
+begin
+  EnterWasPressed := False;
+  if not GxOtaTryGetCurrentSourceEditor(SourceEditor) then
+    Exit;
+  if FCodeList.Count = 1 then
+    GxOtaReplaceSelection(SourceEditor, 0, TrimRight(FCodeList.Text))
+  else begin
+    LineEndType := BlockSelectionToLineEndType(SourceEditor, FCodeList.Text);
+    if LineEndType = 2 then
+      GxOtaReplaceSelection(SourceEditor, 0, Copy(FCodeList.Text, 1, Length(FCodeList.Text) - 2))
+    else if LineEndType = 1 then
+      GxOtaReplaceSelection(SourceEditor, 0, Copy(FCodeList.Text, 1, Length(FCodeList.Text) - 1))
+    else
+      GxOtaReplaceSelection(SourceEditor, 0, FCodeList.Text);
+  end;
 end;
 
 function TChangeCaseExpert.HasConfigOptions: Boolean;
@@ -78,42 +180,88 @@ begin
   Result := False;
 end;
 
-function TChangeCaseExpert.ProcessSelected(Lines: TStrings): Boolean;
-var
-  Index: Integer;
-  Dialog: TfmChangeCase;
+function TChangeCaseExpert.IsEditorActive(out ctl: TWinControl): Boolean;
 begin
-  Result := False;
-  if Lines.Count > 0 then
-  begin
-    Dialog := TfmChangeCase.Create(nil);
-    try
-      if (FLastSelection > -1) and (FLastSelection < Dialog.grpCaseSelection.Items.Count) then
-        Dialog.grpCaseSelection.ItemIndex := FLastSelection;
-      if Dialog.ShowModal = mrOk then
-      begin
-        Index := Dialog.grpCaseSelection.ItemIndex;
+  ctl := Screen.ActiveControl;
+  Result := Assigned(ctl) and (ctl.Name = 'Editor') and ctl.ClassNameIs('TEditControl');
+end;
 
-        case Index of
-          0: Lines.Text := AnsiLowerCase(Lines.Text);
-          1: Lines.Text := AnsiUpperCase(Lines.Text);
-          2: Lines.Text := TitleCase(Lines.Text);
-          3: Lines.Text := SentenceCase(Lines.Text);
-          4: Lines.Text := ToggleCase(Lines.Text);
-        else
-          Exit;
-        end;
-        FLastSelection := Index;
-        Result := True;
-      end;
-    finally
-      FreeAndNil(Dialog);
-    end;
+procedure TChangeCaseExpert.miChangeToLowerCaseClicked(Sender: TObject);
+begin
+  FCodeList.Text := AnsiLowerCase(FCodeList.Text);
+  ReplaceSelection;
+  FLastSelection := (Sender as TMenuItem).MenuIndex;
+end;
+
+procedure TChangeCaseExpert.miChangeToSentenceCaseClicked(Sender: TObject);
+begin
+  FCodeList.Text := SentenceCase(FCodeList.Text);
+  ReplaceSelection;
+  FLastSelection := (Sender as TMenuItem).MenuIndex;
+end;
+
+procedure TChangeCaseExpert.miChangeToTitleCaseClicked(Sender: TObject);
+begin
+  FCodeList.Text := TitleCase(FCodeList.Text);
+  ReplaceSelection;
+  FLastSelection := (Sender as TMenuItem).MenuIndex;
+end;
+
+procedure TChangeCaseExpert.miChangeToUpperCaseClicked(Sender: TObject);
+begin
+  FCodeList.Text := AnsiUpperCase(FCodeList.Text);
+  ReplaceSelection;
+  FLastSelection := (Sender as TMenuItem).MenuIndex;
+end;
+
+procedure TChangeCaseExpert.miToggleCaseClicked(Sender: TObject);
+begin
+  FCodeList.Text := ToggleCase(FCodeList.Text);
+  ReplaceSelection;
+  FLastSelection := (Sender as TMenuItem).MenuIndex;
+end;
+
+procedure TChangeCaseExpert.Execute(Sender: TObject);
+var
+  ctl: TWinControl;
+  pnt: TPoint;
+  SourceEditor: IOTASourceEditor;
+begin
+  if not IsEditorActive(ctl) then
+    Exit;
+  if not GxOtaTryGetCurrentSourceEditor(SourceEditor) then
+    Exit;
+  FCodeList.Text := GxOtaGetCurrentSelection;
+  if FCodeList.Count = 0 then begin
+    GxOtaSelectCurrentIdent(SourceEditor, False);
+    FCodeList.Text := GxOtaGetCurrentSelection;
   end;
+  if FCodeList.Count = 0 then
+    Exit;
+
+  FPopup.Items[FLastSelection].Default := True;
+  if not Windows.GetCaretPos(pnt) then
+    pnt := Point(0, 0);
+  pnt := ctl.ClientToScreen(pnt);
+  EnterWasPressed := False;
+  FPopup.Popup(pnt.X, pnt.Y);
+  // This is a rather ugly hack to execute the default menu item when the user presses OK:
+  // We installed a keyboard hook that sets the global variable EnterWasPressed to true,
+  // when (you guessed it:) the Enter key was pressed. Pressing Enter closes the popup
+  // menu even if no item was selected. EnterWasPressed is set to false just before
+  // showing the popup menu and also in ReplaceSelection which is called by all
+  // menu item's OnClick handlers. So if EnterWasPressed is true, we know:
+  // 1. Enter was pressed after the popup menu was shown
+  // 2. No menu item was executed
+  // so we execute the default item.
+  // I said it was ugly, didn't I?
+  // If you have got any better idea, please feel free to change this code.
+  // -- 2016-10-01 twm
+  if EnterWasPressed then
+    FPopup.Items[FLastSelection].Click;
 end;
 
 initialization
   RegisterEditorExpert(TChangeCaseExpert);
-
 end.
 
