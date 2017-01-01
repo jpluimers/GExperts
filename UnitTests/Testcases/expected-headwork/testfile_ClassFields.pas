b1f166f6a34bb1bab7735e4eee747fec4068494e
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2017-01-01 17:44:15 +0000

    corrected expected file for ClassFields test (newly added)
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@1845 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- UnitTests/Testcases/expected-headwork/testfile_ClassFields.pas
+++ UnitTests/Testcases/expected-headwork/testfile_ClassFields.pas
@@ -5,14 +5,13 @@ interface
 type
   TMyClass = class
   private
-  class var
-    FRed: Integer;
-    FGreen: Integer;
-    FBlue: Integer;
+    class var
+      FRed: Integer;
+      FGreen: Integer;
+      FBlue: Integer;
   public
   end;
 
 implementation
 
 end.
-
