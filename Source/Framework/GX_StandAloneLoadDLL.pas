fbef14f1efd1d0df560ef82e71dcaf737b6ebc18
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2017-02-25 17:16:06 +0000

    new function LoadAnyGExpertsDLL which tries to load any GExperts DLL starting with the one for the most recent Delphi version
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@1968 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- Source/Framework/GX_StandAloneLoadDLL.pas
+++ Source/Framework/GX_StandAloneLoadDLL.pas
@@ -22,6 +22,7 @@ type
   end;
 
 function LoadGExpertsDll(const _DllName: string): IGExpertsDll;
+function LoadAnyGExpertsDLL: IGExpertsDll;
 
 implementation
 
@@ -39,6 +40,40 @@ type
 function LoadGExpertsDll(const _DllName: string): IGExpertsDll;
 begin
   Result := TGExpertsDll.Create(_DllName);
+end;
+
+function TryLoad(const _DllName: string; out _Dll: IGExpertsDll): Boolean;
+begin
+  try
+    _Dll := LoadGExpertsDll(_DllName);
+    Result := True;
+  except
+    Result := False;
+  end;
+end;
+
+function LoadAnyGExpertsDLL: IGExpertsDll;
+begin
+  if TryLoad('GExpertsRS101.dll', Result)
+    or TryLoad('GExpertsRS10.dll', Result)
+    or TryLoad('GExpertsRSXE8.dll', Result)
+    or TryLoad('GExpertsRSXE7.dll', Result)
+    or TryLoad('GExpertsRSXE6.dll', Result)
+    or TryLoad('GExpertsRSXE5.dll', Result)
+    or TryLoad('GExpertsRSXE4.dll', Result)
+    or TryLoad('GExpertsRSXE3.dll', Result)
+    or TryLoad('GExpertsRSXE2.dll', Result)
+    or TryLoad('GExpertsRSXE1.dll', Result)
+    or TryLoad('GExpertsRS2010.dll', Result)
+    or TryLoad('GExpertsRS2009.dll', Result)
+    or TryLoad('GExpertsDelphi2007.dll', Result)
+    or TryLoad('GExpertsBDS2006.dll', Result)
+    or TryLoad('GExpertsDelphi2005.dll', Result)
+//    or TryLoad('GExpertsD8.dll', Result)
+    or TryLoad('GExpertsD7.dll', Result)
+    or TryLoad('GExpertsD6.dll', Result) then
+    Exit; //==>
+  raise Exception.Create('Failed to load any of the possible GExperts DLLs');
 end;
 
 { TGExpertsDll }
