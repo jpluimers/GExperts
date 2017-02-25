8f5264c2a9802a9e5e04f74638bf44a32b194ce9
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2017-02-25 17:51:45 +0000

    * GX_StandAloneLoadDLL to load any GExperts dll * updated paths and options
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@1972 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- Projects/StandAlone/CodeLibrarian/CodeLibrarian.dpr
+++ Projects/StandAlone/CodeLibrarian/CodeLibrarian.dpr
@@ -3,10 +3,30 @@ program CodeLibrarian;
 {$R *_icon.res}
 
 uses
-  GX_VerDepConst in '..\..\Framework\GX_VerDepConst.pas';
+  SysUtils,
+  Forms,
+  GX_StandAloneLoadDLL in '..\..\..\Source\Framework\GX_StandAloneLoadDLL.pas';
 
-procedure ShowCodeLib; external GExpertsDll;
+procedure Main;
+type
+  TShowCodeLib = procedure; // not cdecl, but it doesn't really matter since it dosn't have parameters.
+var
+  ShowCodeLib: TShowCodeLib;
+  Dll: IGExpertsDll;
+begin
+  try
+    Dll := LoadAnyGExpertsDLL;
+    ShowCodeLib := Dll.GetProcAddress('ShowCodeLib');
+    ShowCodeLib;
+    // it worked halt the program
+    Halt(0);
+  except
+    on e: Exception do begin
+      Application.ShowException(e);
+    end;
+  end;
+end;
 
 begin
-  ShowCodeLib;
+  Main;
 end.
