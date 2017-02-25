3037cb4e337ef4be595af9e416a971ac95b51edc
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2017-02-25 12:09:30 +0000

    we no longer support Delphi 8
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@1939 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- BuildInstall/CompileDelphi8.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-SET SRCDIR=D:\Projects\GExperts\Source\
-SET SYNEDIT=D:\Projects\GExperts\ExternalSource\UniSynEdit
-SET DELPHIDIR=D:\IDE\Delphi 7\d8bin
-SET SWITCHES=-B -DGX_AutomatedBuild -DSYNEDIT -LUdesignide -LUvcl -LUvclx -LUrtl -LUvcljpg -$D- -$L- -$Q+ -$R+ -$T+ -I..\Source\Framework -U"D:\IDE\Delphi 7\d8lib" -U..\ExternalSource -U..\ExternalSource\Abbrevia -U%SYNEDIT%
-CD %SRCDIR%
-
-"%DELPHIDIR%\dcc32.exe" %SWITCHES% GExpertsD8.dpr
