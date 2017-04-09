23952df9a9c64dd39bbd34bcfc364e2132b62c2f
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2017-04-09 13:26:44 +0000

    * Simplified code for getting the display label. The panel is simply its parent. * redirect system32 to SysWOW64 for packages located there
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@2061 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- Source/IDE/GX_IdeInstallPackagesEnhancer.pas
+++ Source/IDE/GX_IdeInstallPackagesEnhancer.pas
@@ -1,5 +1,7 @@
 unit GX_IdeInstallPackagesEnhancer;
 
+{$I GX_CondDefine.inc}
+
 interface
 
 uses
@@ -30,28 +32,16 @@ uses
   GX_IdeFormEnhancer,
   GX_GenericUtils,
   GX_IdeUtils,
-  GX_IdeDialogEnhancer;
+  GX_IdeDialogEnhancer,
+  GX_dzClassUtils;
 
 type
   TInstallPackagesEnhancer = class(TIdeDialogEnhancer)
   private
-    FPkgListBox: TCheckListBox;
+//    FPkgListBox: TCheckListBox;
     FPkgNameLabel: TLabel;
-    function IsProjectOptionsForm(_Form: TCustomForm): Boolean;
-    function IsPackageChecklistForm(_Form: TCustomForm): Boolean;
-    procedure EnhancePackageChecklistForm(_Form: TCustomForm);
-    procedure EnhanceProjectOptionsForm(_Form: TCustomForm);
-    function TryGetPackageInstallControlsOnProjectOptions(_Form: TCustomForm;
-      out _grp: TGroupBox; out _AddBtn: TButton; out _ChkLst: TCheckListBox;
-      out _NamePnl: TPanel; out _NameLabel: TLabel): Boolean;
 //    procedure b_RenameClick(_Sender: TObject);
     procedure b_SelectClick(_Sender: TObject);
-    function TryGetPackageInstallControlsOnPackageChecklist(_Form: TCustomForm;
-      out _BtnPanel: TPanel; out _AddBtn: TButton; out _ChkLst: TCheckListBox;
-      out _NamePnl: TPanel;
-      out _NameLabel: TLabel): Boolean;
-    procedure doEnhanceForm(_Form: TCustomForm; _BtnParent: TWinControl;
-      _AddBtn: TButton; _FilenamePanel: TPanel); overload;
   protected
     function IsDesiredForm(_Form: TCustomForm): Boolean; override;
     procedure EnhanceForm(_Form: TForm); overload; override;
@@ -80,150 +70,61 @@ end;
 { TInstallPackagesEnhancer }
 
 function TInstallPackagesEnhancer.IsDesiredForm(_Form: TCustomForm): Boolean;
+
+  function IsPackageChecklistForm(_Form: TCustomForm): Boolean;
+  const
+    DIALOG_CLASS = 'TDlgPackageCheckList';
+    DIALOG_NAME = 'DlgPackageCheckList';
+  begin
+    Result := (_Form.ClassName = DIALOG_CLASS) and (_Form.Name = DIALOG_NAME);
+  end;
+
+  function IsProjectOptionsForm(_Form: TCustomForm): Boolean;
+  const
+  {$IFNDEF GX_VER185_up} // Delphi 2007 (11; BDS 4)
+    DIALOG_CLASS = 'TProjectOptionsDialog';
+    DIALOG_NAME = 'ProjectOptionsDialog';
+  {$ELSE}
+    DIALOG_CLASS = 'TDelphiProjectOptionsDialog';
+    DIALOG_NAME = 'DelphiProjectOptionsDialog';
+  {$ENDIF}
+  begin
+    Result := (_Form.ClassName = DIALOG_CLASS) and (_Form.Name = DIALOG_NAME);
+  end;
+
 begin
   Result := IsProjectOptionsForm(_Form) or
     IsPackageChecklistForm(_Form);
 end;
 
 procedure TInstallPackagesEnhancer.EnhanceForm(_Form: TForm);
-begin
-  if IsProjectOptionsForm(_Form) then begin
-    EnhanceProjectOptionsForm(_Form);
-  end else if IsPackageChecklistForm(_Form) then begin
-    EnhancePackageChecklistForm(_Form);
-  end;
-end;
-
-function TInstallPackagesEnhancer.IsPackageChecklistForm(_Form: TCustomForm): Boolean;
-begin
-  Result := (_Form.ClassName = 'TDlgPackageCheckList') and (_Form.Name = 'DlgPackageCheckList');
-end;
-
-function TInstallPackagesEnhancer.IsProjectOptionsForm(_Form: TCustomForm): Boolean;
-begin
-  Result := (_Form.ClassName = 'TDelphiProjectOptionsDialog') and (_Form.Name = 'DelphiProjectOptionsDialog')
-    or (_Form.ClassName = 'TProjectOptionsDialog') and (_Form.Name = 'ProjectOptionsDialog');
-end;
-
-function TInstallPackagesEnhancer.TryGetPackageInstallControlsOnProjectOptions(_Form: TCustomForm;
-  out _grp: TGroupBox; out _AddBtn: TButton; out _ChkLst: TCheckListBox;
-  out _NamePnl: TPanel; out _NameLabel: TLabel): Boolean;
-var
-  ctrl: TControl;
-  wctrl: TWinControl;
-  i: Integer;
-  PackageInstall: TWinControl;
-begin
-  Result := False;
-
-  wctrl := nil;
-  for i := 0 to _Form.ControlCount - 1 do begin
-    ctrl := _Form.Controls[i];
-    if (ctrl.ClassName = 'TPanel') and (ctrl.Name = 'Panel2') then begin
-      wctrl := TWinControl(ctrl);
-      Break;
-    end;
-  end;
-  if not Assigned(wctrl) then
-    Exit;
-
-  if (wctrl.ControlCount > 0) and (wctrl.Controls[0].ClassName = 'TPropertySheetControl') then
-    ctrl := wctrl.Controls[0]
-  else if (wctrl.ControlCount > 1) and (wctrl.Controls[1].ClassName = 'TPropertySheetControl') then
-    ctrl := wctrl.Controls[1]
-  else
-    Exit;
-  wctrl := TWinControl(ctrl);
-
-  PackageInstall := nil;
-  for i := 0 to wctrl.ControlCount - 1 do begin
-    ctrl := wctrl.Controls[i];
-    if ctrl.ClassName = 'TPackageInstall' then begin
-      PackageInstall := TWinControl(ctrl);
-      Break;
-    end;
-  end;
-  if (PackageInstall = nil) or (PackageInstall.ControlCount < 1) then
-    Exit;
-  ctrl := PackageInstall.Controls[0];
-  if ctrl.ClassName <> 'TGroupBox' then
-    Exit;
-  _grp := TGroupBox(ctrl);
-
-  if _grp.ControlCount <> 6 then
-    Exit;
-
-  ctrl := _grp.Controls[0];
-  if (ctrl.ClassName <> 'TCheckListBox') or (ctrl.Name <> 'DesignPackageList') then
-    Exit;
-  _ChkLst := TCheckListBox(ctrl);
-
-  ctrl := _grp.Controls[2];
-  if (ctrl.ClassName <> 'TButton') or (ctrl.Name <> 'AddButton') then
-    Exit;
-  _AddBtn := TButton(ctrl);
-
-  ctrl := _grp.Controls[1];
-  if (ctrl.ClassName <> 'TPanel') or (ctrl.Name <> 'Panel1') then
-    Exit;
-
-  _NamePnl := TPanel(ctrl);
-  if _NamePnl.ControlCount < 1 then
-    Exit;
-  ctrl := _NamePnl.Controls[0];
-  if (ctrl.ClassName <> 'TLabel') or (ctrl.Name <> 'LabelPackageFile') then
-    Exit;
-  _NameLabel := TLabel(ctrl);
-
-  Result := True;
-end;
-
-// when called via Component -> Install Packages and a project is loaded
-// or via Project -> Options:
-// DelphiProjectOptionsDialog !or! ProjectOptionsDialog: TDelphiProjectOptionsDialog !or! TProjectOptionsDialog
-// -> [1, 2 or 3] Panel2: TPanel
-//    -> [0 or 1] PropertySheetControl1 !or! PropSheetControl: TpropertySheetControl
-//       -> [??] '': TPackageInstall
-//          -> [0] GroupBox1: TGroupBox !or! [0] gbDesignPackages: TGroupBox
-//             -> [0] DesignPackageList: TCheckListBox
-//             -> [1] Panel1: TPanel
-//                -> [0] LabelPackageFile: TLabel
-//             -> [2] AddButton: TButton
-//             -> [3] RemoveButton: TButton
-//             -> [4] ButtonComponents: TButton
-//             -> [5] EditButton: TButton
-
-procedure TInstallPackagesEnhancer.doEnhanceForm(_Form: TCustomForm; _BtnParent: TWinControl;
-  _AddBtn: TButton; _FilenamePanel: TPanel);
 resourcestring
   rcSelectAModule = 'Open an explorer window with this package selected.';
 const
-  ExplorerButtonName = 'GXExplorerButton';
+  ExplorerButtonName = 'GXPackageExplorerButton';
 var
-//  RenameBtn: TButton;
   SelectBtn: TButton;
   cmp: TComponent;
+  NamePnl: TPanel;
 begin
 // Unfortunately renaming packages (see b_RenameClick method) does not work.
 // So we don't add the Rename button.
-//  RenameBtn := TButton.Create(_BtnParent);
-//  RenameBtn.Name := '';
-//  RenameBtn.Parent := _BtnParent;
-//  RenameBtn.Left := _AddBtn.Left - RenameBtn.Width - 8;
-//  RenameBtn.Top := _AddBtn.Top;
-//  RenameBtn.Caption := 'Rename';
-//  RenameBtn.Anchors := _AddBtn.Anchors;
-//  RenameBtn.TabOrder := _AddBtn.TabOrder;
-//  RenameBtn.OnClick := b_RenameClick;
 
-  cmp := _FilenamePanel.FindComponent(ExplorerButtonName);
-  if Assigned(cmp) then
+  if TComponent_FindComponent(_Form, ExplorerButtonName, True, cmp, TButton) then
     Exit; // the button already exists
 
-  SelectBtn := TButton.Create(_FilenamePanel);
+  if not TComponent_FindComponent(_Form, 'LabelPackageFile', True, TComponent(FPkgNameLabel), TLabel) then
+    Exit; // label not found, so we can't add the button
+
+  if not (FPkgNameLabel.Parent is TPanel) then
+    Exit; // the label should be on a panel
+
+  NamePnl := TPanel(FPkgNameLabel.Parent);
+
+  SelectBtn := TButton.Create(NamePnl);
   SelectBtn.Name := ExplorerButtonName;
-  SelectBtn.Parent := _FilenamePanel;
-  SelectBtn.Width := _FilenamePanel.ClientHeight;
+  SelectBtn.Parent := NamePnl;
+  SelectBtn.Width := NamePnl.ClientHeight;
   SelectBtn.Caption := '...';
   SelectBtn.Align := alRight;
   SelectBtn.OnClick := b_SelectClick;
@@ -231,21 +132,21 @@ begin
   SelectBtn.ShowHint := True;
 end;
 
-procedure TInstallPackagesEnhancer.EnhanceProjectOptionsForm(_Form: TCustomForm);
-var
-  grp: TGroupBox;
-  pnl: TPanel;
-  AddBtn: TButton;
-begin
-  if not TryGetPackageInstallControlsOnProjectOptions(_Form, grp, AddBtn, FPkgListBox, pnl, FPkgNameLabel) then
-    Exit;
-  doEnhanceForm(_Form, grp, AddBtn, pnl);
-end;
-
 procedure TInstallPackagesEnhancer.b_SelectClick(_Sender: TObject);
+var
+  fn: string;
+  Wow64fn: string;
 begin
-  // open an explrer window and select the package file there
-  ShellExecute(0, nil, 'explorer.exe', PChar('/select,' + FPkgNameLabel.Caption), nil, SW_SHOWNORMAL)
+  fn := FPkgNameLabel.Caption;
+  if fn = '' then
+    Exit;
+
+  Wow64fn := StringReplace(fn, '\system32\', '\SysWOW64\', [rfIgnoreCase]);
+  if FileExists(Wow64fn) then
+    fn := Wow64fn;
+
+  // open an explorer window and select the package file there
+  ShellExecute(0, nil, 'explorer.exe', PChar('/select,' + fn), nil, SW_SHOWNORMAL)
 end;
 
 //procedure TInstallPackagesEnhancer.b_RenameClick(_Sender: TObject);
@@ -292,89 +193,5 @@ end;
 //    Reg.Free;
 //  end;
 //end;
-
-procedure TInstallPackagesEnhancer.EnhancePackageChecklistForm(_Form: TCustomForm);
-var
-  BtnParent: TPanel;
-  FilenamePanel: TPanel;
-  AddBtn: TButton;
-begin
-  if not TryGetPackageInstallControlsOnPackageChecklist(_Form, BtnParent, AddBtn, FPkgListBox, FilenamePanel, FPkgNameLabel) then
-    Exit;
-  doEnhanceForm(_Form, BtnParent, AddBtn, FilenamePanel);
-end;
-
-function TInstallPackagesEnhancer.TryGetPackageInstallControlsOnPackageChecklist(_Form: TCustomForm;
-  out _BtnPanel: TPanel; out _AddBtn: TButton; out _ChkLst: TCheckListBox;
-  out _NamePnl: TPanel; out _NameLabel: TLabel): Boolean;
-var
-  ctrl: TControl;
-  wctrl: TWinControl;
-begin
-  Result := False;
-
-  if _Form.ControlCount < 4 then
-    Exit;
-
-  ctrl := _Form.Controls[3];
-  if ctrl.ClassName <> 'TGroupBox' then
-    Exit;
-  wctrl := TGroupBox(ctrl);
-
-  if wctrl.ControlCount < 1 then
-    Exit;
-
-  ctrl := wctrl.Controls[0];
-  if (ctrl.ClassName <> 'TFramePackageCheckList') or (ctrl.Name <> 'FramePackageCheckList1') then
-    Exit;
-  wctrl := TWinControl(ctrl);
-
-  if wctrl.ControlCount < 1 then
-    Exit;
-  ctrl := wctrl.Controls[0];
-  if (ctrl.ClassName <> 'TPanel') and (ctrl.Name <> 'Panel2') then
-    Exit;
-  wctrl := TWinControl(ctrl);
-  _BtnPanel := TPanel(wctrl);
-
-  if wctrl.ControlCount < 6 then
-    Exit;
-
-  ctrl := wctrl.Controls[1];
-  if (ctrl.ClassName <> 'TCheckListBox') or (ctrl.Name <> 'DesignPackageList') then
-    Exit;
-  _ChkLst := TCheckListBox(ctrl);
-
-  ctrl := wctrl.Controls[2];
-  if (ctrl.ClassName <> 'TButton') or (ctrl.Name <> 'AddButton') then
-    Exit;
-  _AddBtn := TButton(ctrl);
-
-  ctrl := wctrl.Controls[0];
-  if (ctrl.ClassName <> 'TPanel') or (ctrl.Name <> 'Panel1') then
-    Exit;
-
-  _NamePnl := TPanel(ctrl);
-  if _NamePnl.ControlCount < 1 then
-    Exit;
-  ctrl := _NamePnl.Controls[0];
-  if (ctrl.ClassName <> 'TLabel') or (ctrl.Name <> 'LabelPackageFile') then
-    Exit;
-  _NameLabel := TLabel(ctrl);
-
-  Result := True;
-end;
-// when called via Component -> Install Packages and no project is loaded:
-// DlgPackageCheckList: TDlgPackageCheckList
-// -> [3] GroupBox1: TGroupBox
-//    -> [0] FramePackageCheckList1: TFramePackageCheckList
-//       -> [0] Panel2: TPanel
-//          -> [0] Panel1: TPanel
-//             -> [0] LabelPackageFile: TLabel
-//          -> [1] DesignPackageList: TCheckListBox
-//          -> [2] AddButton: TButton
-//          -> [3] RemoveButton: TButton
-//          -> [4] ButtonComponents: TButton
-//          -> [5] EditButton: TButton
 
 end.
