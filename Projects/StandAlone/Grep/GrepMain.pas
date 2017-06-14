a12d4d35a09e1d8782c3001adfcd5df428eec28f
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2017-06-14 10:22:33 +0000

    * SetErrorMode(SEM_FAILCRITICALERRORS) to prevent system error dialog when the DLL cannot be loaded becaus the RTLxxx.bpl package is not available * If a parameter is passed, assume it to be a directory and call ShowGrepEx instead of ShowGrep.
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@2078 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- Projects/StandAlone/Grep/GrepMain.pas
+++ Projects/StandAlone/Grep/GrepMain.pas
@@ -7,22 +7,41 @@ procedure Main;
 implementation
 
 uses
+  Windows,
   SysUtils,
   Classes,
   Forms,
   GX_StandAloneLoadDLL;
 
 procedure Main;
+const
+{$IFDEF unicode}
+  ShowGrepExName = 'ShowGrepExW';
+{$ELSE}
+  ShowGrepExName = 'ShowGrepExA';
+{$ENDIF}
 type
   TShowGrep = procedure;
+{$IFDEF unicode}
+  TShowGrepEx = procedure(const _Directory: PWideChar);
+{$ELSE}
+  TShowGrepEx = procedure(const _Directory: PAnsiChar);
+{$ENDIF}
 var
   ShowGrep: TShowGrep;
+  ShowGrepEx: TShowGrepEx;
   Dll: IGExpertsDll;
 begin
   try
+    SetErrorMode(SEM_FAILCRITICALERRORS);
     Dll := LoadAnyGExpertsDLL;
-    ShowGrep := Dll.GetProcAddress('ShowGrep');
-    ShowGrep;
+    if ParamCount > 0 then begin
+      ShowGrepEx := Dll.GetProcAddress(ShowGrepExName);
+      ShowGrepEx(PChar(ParamStr(1)));
+    end else begin
+      ShowGrep := Dll.GetProcAddress('ShowGrep');
+      ShowGrep;
+    end;
     // it worked halt the program
     Halt(0);
   except
