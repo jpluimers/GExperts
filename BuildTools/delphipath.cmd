c8c2c8738d5bc78643bbd41a8ed7117f1ea7916f
dejanm <dejanm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2017-09-02 10:05:07 +0000

    Bugfix: Didn't work correctly for Delphi 10.2
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@2114 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- BuildTools/delphipath.cmd
+++ BuildTools/delphipath.cmd
@@ -18,55 +18,58 @@ call :Delphi%DelphiVersion%
 goto DelphiEndCase
 :Delphi6
   call :ReadReg Borland\Delphi\6.0
-  goto :eof
+  goto :DelphiEndCase
 :Delphi7
   call :ReadReg Borland\Delphi\7.0
-  goto :eof
+  goto :DelphiEndCase
 :Delphi2005
   call :ReadReg Borland\BDS\3.0
-  goto :eof
+  goto :DelphiEndCase
 :Delphi2006
   call :ReadReg Borland\BDS\4.0
-  goto :eof
+  goto :DelphiEndCase
 :Delphi2007
   call :ReadReg Borland\BDS\5.0
-  goto :eof
+  goto :DelphiEndCase
 :Delphi2009
   call :ReadReg CodeGear\BDS\6.0
-  goto :eof
+  goto :DelphiEndCase
 :Delphi2010
   call :ReadReg CodeGear\BDS\7.0
-  goto :eof
+  goto :DelphiEndCase
 :DelphiXE
   call :ReadReg Embarcadero\BDS\8.0
-  goto :eof
+  goto :DelphiEndCase
 :DelphiXE2
   call :ReadReg Embarcadero\BDS\9.0
-  goto :eof
+  goto :DelphiEndCase
 :DelphiXE3
   call :ReadReg Embarcadero\BDS\10.0
-  goto :eof
+  goto :DelphiEndCase
 :DelphiXE4
   call :ReadReg Embarcadero\BDS\11.0
-  goto :eof
+  goto :DelphiEndCase
 :DelphiXE5
   call :ReadReg Embarcadero\BDS\12.0
-  goto :eof
+  goto :DelphiEndCase
 :DelphiXE6
   call :ReadReg Embarcadero\BDS\14.0
-  goto :eof
+  goto :DelphiEndCase
 :DelphiXE7
   call :ReadReg Embarcadero\BDS\15.0
-  goto :eof
+  goto :DelphiEndCase
 :DelphiXE8
   call :ReadReg Embarcadero\BDS\16.0
-  goto :eof
+  goto :DelphiEndCase
 :Delphi10Seattle
   call :ReadReg Embarcadero\BDS\17.0
+  goto :DelphiEndCase
 :Delphi101Berlin
   call :ReadReg Embarcadero\BDS\18.0
+  goto :DelphiEndCase
 :Delphi102
-  call :ReadReg Embarcadero\BDS\18.0
+  call :ReadReg Embarcadero\BDS\19.0
+  goto :DelphiEndCase
 :DelphiEndCase
 
 echo DelphiPath: "%DelphiPath%"
@@ -83,7 +86,7 @@ goto :eof
 :ReadReg
 rem read the registry entry
 set DelphiPath=
-FOR /F "usebackq skip=2 tokens=3,*" %%A IN (`REG QUERY HKCU\Software\%1 /v RootDir 2^>nul`) DO (
+FOR /F "usebackq skip=2 tokens=3,*" %%A IN (`REG QUERY "HKCU\Software\%~1" /v RootDir 2^>nul`) DO (
   set DelphiPath=%%A %%B
 )
 rem remove one trailing space which might have been added because %%B was empty
