b5390cead575bbc67a3047da5b7ce5e00bb95680
jeroen <jeroen@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2017-10-14 17:09:14 +0000

    Patches submitted by Jeroen: for the following issues: 1 https://github.com/jpluimers/GExperts/issues/3  Allow delete key in grep search `Results` pane to delete an entry  The popup menu for the Results pane has a Delete action bound to it, but it has no keyboard shortcut.  Add a shortcut for the Del key, then verify it deletes the correct entry.
    2 https://github.com/jpluimers/GExperts/issues/2
     Grep search makes IDE unresponsive when searching massive amounts of directories with non-matching files
     If the grep search goes through a massive amounts of directories with non-matching files (for instance when
     initiating  a recursive search for anything Pascal like from C:), then:
       1. the IDE becomes sluggish and often unresponsive
       2. the IDE does not show where the search currently is inside the directory tree
     Proposed solution is to add a TfmGrepResults.StartDirectorySearch similar to TfmGrepResults.StartFileSearch which
     updates the UI and performs an Application.ProcessMessages.
     Then hook this method to a new event TGrepSearchRunner.OnSearchDirectory which works similar to
     TGrepSearchRunner.OnSearchFile, but on the directory level performing a call-back right after the FindFirst occurences
     1 and 2, but not inside the while loop repetitions.
     If the situation stays sluggish or unresponsive, then move the call-back inside the while loop repetitions.
    
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@2120 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- Source/Grep/GX_GrepResults.pas
+++ Source/Grep/GX_GrepResults.pas
@@ -271,10 +271,13 @@ type
     FSaveItemEmptyCaption: string;
     FSaveSortCaption: string;
     FSavedFormCaption: string;
+    FlbHistoryListIndexForHistoryMenuActions: Integer;
     procedure SetStayOnTop(Value: Boolean);
     procedure RefreshContextLines;
     procedure SetShowContext(Value: Boolean);
     procedure HighlightMemo(FileMatches: TFileResult; StartLine, MatchLineNo: Integer);
+    procedure ReportProgress(const PathName: string);
+    procedure StartDirectorySearch(Sender: TObject; const DirectoryName: string);
     procedure StartFileSearch(Sender: TObject; const FileName: string);
     procedure SaveSettings;
     procedure LoadSettings;
@@ -308,6 +311,7 @@ type
     procedure ClearResultsData;
     procedure DoEmbeddedSearch(Sender: TObject);
     procedure UpdateHistoryPagesOptions;
+    property lbHistoryListIndexForHistoryMenuActions: Integer read FlbHistoryListIndexForHistoryMenuActions write FlbHistoryListIndexForHistoryMenuActions;
   protected
     procedure CreateParams(var Params: TCreateParams); override;
     procedure AssignSettingsToForm;
@@ -439,14 +443,14 @@ begin
   end;
 end;
 
-procedure TfmGrepResults.StartFileSearch(Sender: TObject; const FileName: string);
+procedure TfmGrepResults.ReportProgress(const PathName: string);
 resourcestring
   SProcessing = 'Processing %s';
 var
   Dummy: Boolean;
   CurrentGetTickCount: DWORD;
 begin
-  SetStatusString(Format(SProcessing, [FileName]));
+  SetStatusString(Format(SProcessing, [PathName]));
   ActionsUpdate(nil, Dummy);
   CurrentGetTickCount := GetTickCount;
   if CurrentGetTickCount <> FLastRepaintTick then
@@ -454,6 +458,16 @@ begin
     Application.ProcessMessages;
     FLastRepaintTick := CurrentGetTickCount;
   end;
+end;
+
+procedure TfmGrepResults.StartDirectorySearch(Sender: TObject; const DirectoryName: string);
+begin
+  ReportProgress(DirectoryName);
+end;
+
+procedure TfmGrepResults.StartFileSearch(Sender: TObject; const FileName: string);
+begin
+  ReportProgress(FileName);
 end;
 
 function TfmGrepResults.Execute(AState: TGrepSearchState): Boolean;
@@ -508,6 +522,7 @@ begin
 
     try
       FSearcher.OnSearchFile := StartFileSearch;
+      FSearcher.OnSearchDirectory := StartDirectorySearch;
       FSearchInProgress := True;
       FSearcher.Execute;
       FilesSearched := FSearcher.FileSearchCount;
@@ -2069,6 +2084,7 @@ begin
     AIndex := lbHistoryList.Count-1;
 
   lbHistoryList.ItemIndex := AIndex;
+  lbHistoryListIndexForHistoryMenuActions := AIndex; // in case actHistroy... actions get fired without pmHistoryMenu being visible
   ViewHistoryListItems(AIndex, True);
 
   AItem := GrepExpert.HistoryList.Items[AIndex];
@@ -2113,7 +2129,7 @@ begin
           gossaShowSearchWindow:
           begin
             ViewHistoryListItems(ClickedEntry, False);
-            pmHistoryMenu.Tag := ClickedEntry;
+            lbHistoryListIndexForHistoryMenuActions := ClickedEntry;
             actHistorySearch.Execute;
           end ;
           gossaShowEmbeddedSearch:
@@ -2124,7 +2140,7 @@ begin
           end;
           gossaAutoRefresh:
           begin
-            pmHistoryMenu.Tag := ClickedEntry;
+            lbHistoryListIndexForHistoryMenuActions := ClickedEntry;
             actHistoryRefresh.Execute;
           end;
           //Auto refresh when double click
@@ -2152,7 +2168,7 @@ begin
         //Auto refresh when double click
         gossaAutoRefreshDouble:
         begin
-          pmHistoryMenu.Tag := ClickedEntry;
+          lbHistoryListIndexForHistoryMenuActions := ClickedEntry;
           actHistoryRefresh.Execute;
         end;
       end;
@@ -2179,7 +2195,7 @@ begin
   if AIndex = -1 then
     AIndex := lbHistoryList.ItemIndex;
 
-  pmHistoryMenu.Tag := AIndex;
+  lbHistoryListIndexForHistoryMenuActions := AIndex;
 
   if AIndex = -1 then
   begin
@@ -2255,14 +2271,14 @@ procedure TfmGrepResults.miHistoryItemNameClick(Sender: TObject);
 var
   AItem: TGrepHistoryListItem;
 begin
-  AItem := GrepExpert.HistoryList.Items[pmHistoryMenu.Tag];
+  AItem := GrepExpert.HistoryList.Items[lbHistoryListIndexForHistoryMenuActions];
   if Assigned(AItem) then
     Clipboard.AsText := AItem.SearchText;
 end;
 
 procedure TfmGrepResults.actHistoryViewExecute(Sender: TObject);
 begin
-  lbHistoryList.ItemIndex := pmHistoryMenu.Tag;
+  lbHistoryList.ItemIndex := lbHistoryListIndexForHistoryMenuActions;
   ViewHistoryListItems(lbHistoryList.ItemIndex, True);
 end;
 
@@ -2287,7 +2303,7 @@ var
   AItem: TGrepHistoryListItem;
   ASaveSettings: TGrepSettings;
 begin
-  AItem := GrepExpert.HistoryList.Items[pmHistoryMenu.Tag];
+  AItem := GrepExpert.HistoryList.Items[lbHistoryListIndexForHistoryMenuActions];
   if not Assigned(AItem) then
     Exit;
 
@@ -2381,7 +2397,7 @@ var
   AIndex, ATopIndex: Integer;
   IsCurrent: Boolean;
 begin
-  AIndex := pmHistoryMenu.Tag;
+  AIndex := lbHistoryListIndexForHistoryMenuActions;
   if AIndex = -1 then
    Exit;
 
