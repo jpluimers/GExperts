accf7860b3ec41ceee91b647707487182e9568e5
achim <achim@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2017-12-28 16:27:20 +0000

    Sort component names by tab order and then by name (Patch by Achim Kalwa)
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@2125 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- Source/Experts/GX_CopyComponentNames.pas
+++ Source/Experts/GX_CopyComponentNames.pas
@@ -20,13 +20,25 @@ type
 implementation
 
 uses
-  SysUtils, Classes, Clipbrd, ToolsAPI, GX_OtaUtils;
+  SysUtils, Classes, TypInfo, Clipbrd, ToolsAPI, Math, GX_OtaUtils;
 
 { TCopyComponentNamesExpert }
 
 procedure TCopyComponentNamesExpert.UpdateAction(Action: TCustomAction);
 begin
   Action.Enabled := GxOtaFormEditorHasSelectedComponent;
+end;
+
+function InternalComponentSort(List: TStringList; Index1, Index2: Integer): Integer;
+var
+  N1, N2 : Integer;
+begin
+  N1 := Integer(List.Objects[Index1]); // "TabOrder" property value
+  N2 := Integer(List.Objects[Index2]);
+  Result := CompareValue(N1, N2);
+
+  if Result = 0 then
+    Result := CompareText(List.Strings[Index1], List.Strings[Index2]);
 end;
 
 procedure TCopyComponentNamesExpert.Execute(Sender: TObject);
@@ -36,20 +48,41 @@ var
   CurrentComponent: IOTAComponent;
   Names: TStringList;
   ComponentName: WideString;
+  LTypeKind : TTypeKind;
+  LTabOrder : Integer;
   i: Integer;
 begin
   if not GxOtaTryGetCurrentFormEditor(FormEditor) then
     Exit;
   SelCount := FormEditor.GetSelCount;
-  Names := TStringList.Create;  
+  Names := TStringList.Create;
   try
     for i := 0 to SelCount - 1 do
     begin
       CurrentComponent := FormEditor.GetSelComponent(i);
       ComponentName := GxOtaGetComponentName(CurrentComponent);
       if ComponentName <> '' then
-        Names.Add(ComponentName);
+      begin
+        // The order of selected components is not defined, or at least
+        // unexpected. Try to sort the component names by the "TabOrder"
+        // property value:
+        // todo: Since there are many components that do not have a tab order, maybe use
+        //       the YX-position instead? But even then: It is possible to select
+        //       components with different parents, so the list might still be sorted in
+        //       an unexpected way. Maybe sort by YX-Position on the form? That might be best
+        //       but then the sorting is much more complicated because it has to compare two
+        //       values that can not be easily stored in the Object pointer. OTOH why not
+        //       store references to the components there and use GetPropertyValueByName in
+        //       the compare function?
+        LTabOrder := 0;
+        LTypeKind := CurrentComponent.GetPropTypeByName('TabOrder');
+        if LTypeKind = tkInteger then
+          CurrentComponent.GetPropValueByName('TabOrder', LTabOrder);
+
+        Names.AddObject(ComponentName, Pointer(LTabOrder));
+      end;
     end;
+    Names.CustomSort(InternalComponentSort);
     Clipboard.AsText := Trim(Names.Text);
   finally
     FreeAndNil(Names);
