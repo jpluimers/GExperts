c264275b1037130ddabad85552a78df4fd2d9910
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2018-03-31 16:52:39 +0000

    updated version number for installers
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@2192 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- BuildInstall/MakeInstallers.cmd
+++ BuildInstall/MakeInstallers.cmd
@@ -1,7 +1,7 @@
 @echo off
 set iscc="C:\Program Files (x86)\Inno Setup 5\ISCC.exe"
-set version=1.38
-set instver=138
+set version=1.39
+set instver=139
 call :getdate
 set special=-experimental-twm-%dateYYYYMMDD%
 
