efd454cb35d61ac0499efd906d3faa3e6725d531
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2018-05-26 11:26:56 +0000

    some refactoring
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@2284 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- Source/Framework/GX_UsageStatistics.pas
+++ Source/Framework/GX_UsageStatistics.pas
@@ -105,11 +105,8 @@ begin
 end;
 
 procedure TfmUsageStatistics.FillStats;
-var
-  ClassUsageCount: Integer;
-  ClassTotalCount: Integer;
 
-  procedure AppendExpert(_Expert: TGX_BaseExpert; _Items: TListItems);
+  procedure AppendExpert(_Expert: TGX_BaseExpert; _Items: TListItems; var _CurrentCnt, _TotalCnt: Integer);
   var
     li: TListItem;
     cnt: Integer;
@@ -119,74 +116,72 @@ var
       li.Caption := _Expert.GetDisplayName;
       cnt := _Expert.CallCount;
       li.SubItems.Add(IntToStr(cnt));
-      Inc(ClassUsageCount, cnt);
+      Inc(_CurrentCnt, cnt);
       Inc(cnt, _Expert.TotalCallCount);
       li.SubItems.Add(IntToStr(cnt));
-      Inc(ClassTotalCount, cnt);
+      Inc(_TotalCnt, cnt);
     end;
   end;
 
-  procedure AppendTotals(_Items: TListItems);
+  procedure AppendTotals(_Items: TListItems; _CurrentCnt, _TotalCnt: Integer);
   var
     li: TListItem;
   begin
     li := _Items.Add;
     li.Caption := 'Total';
-    li.SubItems.Add(IntToStr(ClassUsageCount));
-    li.SubItems.Add(IntToStr(ClassTotalCount));
+    li.SubItems.Add(IntToStr(_CurrentCnt));
+    li.SubItems.Add(IntToStr(_TotalCnt));
   end;
 
 var
   GExperts: TGExperts;
-  AllUsageCount: Integer;
-  AllTotalCount: Integer;
-  cnt: Integer;
+  ExpCurCnt: Integer;
+  ExpTotalCnt: Integer;
+  EdtCurCnt: Integer;
+  EdtTotalCnt: Integer;
   i: Integer;
   Items: TListItems;
 begin
+  GExperts := GExpertsInst(True);
+
+  ExpCurCnt := 0;
+  ExpTotalCnt := 0;
+  EdtCurCnt := 0;
+  EdtTotalCnt := 0;
+
   Items := lv_Experts.Items;
   Items.BeginUpdate;
   try
-    lv_Experts.Clear;
-    GExperts := GExpertsInst(True);
-
-    ClassUsageCount := 0;
-    ClassTotalCount := 0;
+    Items.Clear;
     for i := 0 to GExperts.ExpertCount - 1 do begin
-      AppendExpert(GExperts.ExpertList[i], Items);
+      AppendExpert(GExperts.ExpertList[i], Items, ExpCurCnt, ExpTotalCnt);
     end;
-    AppendTotals(Items);
-//    TListView_Resize(lv_Experts);
+    AppendTotals(Items, ExpCurCnt, ExpTotalCnt);
   finally
     Items.EndUpdate;
   end;
-
-  AllUsageCount := ClassUsageCount;
-  AllTotalCount := ClassTotalCount;
-  ClassUsageCount := 0;
-  ClassTotalCount := 0;
 
   Items := lv_EditorExperts.Items;
   Items.BeginUpdate;
   try
+    Items.Clear;
     for i := 0 to GExperts.EditorExpertManager.EditorExpertCount - 1 do begin
-      AppendExpert(GExperts.EditorExpertManager.EditorExpertList[i], Items);
+      AppendExpert(GExperts.EditorExpertManager.EditorExpertList[i], Items, EdtCurCnt, EdtTotalCnt);
     end;
-    AppendTotals(Items);
-//    TListView_Resize(lv_EditorExperts);
+    AppendTotals(Items, EdtCurCnt, EdtTotalCnt);
   finally
     Items.EndUpdate;
   end;
 
-  Inc(AllUsageCount, ClassUsageCount);
-  Inc(AllTotalCount, ClassTotalCount);
+  Inc(ExpCurCnt, EdtCurCnt);
+  Inc(ExpTotalCnt, EdtTotalCnt);
 
-  if AllTotalCount = 0 then
+  if ExpTotalCnt = 0 then
     l_Blurb.Visible := False
   else begin
     l_Blurb.Caption := Format(
       'Assuming each usage of an Expert saved you just one second, you have saved %s in this session and %s in total.',
-      [SecondsToHumanReadableString(AllUsageCount), SecondsToHumanReadableString(AllTotalCount)]);
+      [SecondsToHumanReadableString(ExpCurCnt), SecondsToHumanReadableString(ExpTotalCnt)]);
     l_Blurb.Visible := True;
   end;
 end;
