abd8c2e44753d73fb3920a26fa702458335e6d4e
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2018-11-03 19:38:11 +0000

    added new form to all projects
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@2429 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- Projects/Delphi7/GExpertsD7.dpr
+++ Projects/Delphi7/GExpertsD7.dpr
@@ -240,7 +240,8 @@ uses
   GX_KeyboardShortcuts in '..\..\Source\KeyboardShortcuts\GX_KeyboardShortcuts.pas' {fmGxKeyboardShortcuts},
   GX_IdeDockFormEnhancer in '..\..\Source\IDE\GX_IdeDockFormEnhancer.pas',
   GX_AddDockWindow in '..\..\Source\Experts\GX_AddDockWindow.pas' {fmGxDockForm},
-  GX_eRemoveMatchingLines in '..\..\Source\Editor\GX_eRemoveMatchingLines.pas' {fmRemoveMatchingLinesExpertConfig};
+  GX_eRemoveMatchingLines in '..\..\Source\Editor\GX_eRemoveMatchingLines.pas' {fmRemoveMatchingLinesExpertConfig},
+  GX_TabOrderOptions in '..\..\Source\TabOrder\GX_TabOrderOptions.pas' {fmTabOrderOptions};
 
 {$R '..\..\images\GXIcons.res' '..\..\images\GXIcons.rc'}
 {$R '..\..\Binaries\CodeProofreaderDefault.res' '..\..\Binaries\CodeProofreaderDefault.rc'}
