bd820e33224636a821a36f4e51f005a511acaa81
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2018-11-24 19:37:01 +0000

    Bugfix for broken Delphi 10.3 rsvars.bat on my computer (dotNET 4.5 is not installed where rsvars.bat expects it.
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@2459 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- BuildTools/doBuildProject.cmd
+++ BuildTools/doBuildProject.cmd
@@ -19,13 +19,42 @@ if "%delphiver%" == "2006" goto dcc32
 
 @rem build is done with msbuild
 if "%delphiver%" == "2007" goto is2007
+if "%delphiver%" == "Xx103" goto isRio
 
 @rem Delphi 2009+ works fine in 64 bit Windows
 call "%DelphiPath%\bin\rsvars.bat"
 @rem determine the .dproj file
 for %%a in (GExperts*.dproj) do set dprname=%%a
 @rem build it
+:dobuild
 msbuild /target:rebuild /p:config=Release %dprname%
+goto :done
+
+:isRio
+@rem On my machine the Installer created a rsvars.bat which points to the wrong
+@rem directory for the dotNet framework: C:\Windows\Microsoft.NET\Framework\v4.5
+@rem The actual installation is in: C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319
+@rem (This is because MS installed all dotNET updates after 4.0 to the existing directory.)
+set OldPath=%PATH%
+call "%DelphiPath%\bin\rsvars.bat"
+
+@rem if msbuild.exe exists in that directory, everthing is fine
+if exist %FrameworkDir%\msbuild.exe goto :dobuild
+
+@rem The following could probably check the registriy as described in the link below,
+@rem but I am too lazy right now to implement it. Maybe later. For now I simply check for it,
+@rem where it is on my computer, otherwise we simply fail and let the user deal with it.
+if not exist C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe goto :wrongdn
+@rem if it is there, change FrameworkDir and search path and call it.
+SET FrameworkDir=C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319
+set PATH=%FrameworkDir%;%OldPath%
+goto :dobuild
+
+:wrongdn
+echo dotNET %FrameworkVersion% not found on your computer. Check
+echo https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed#to-find-net-framework-versions-by-viewing-the-registry-net-framework-45-and-later
+echo on how to find or install it.
+pause
 goto :done
 
 :is2007
