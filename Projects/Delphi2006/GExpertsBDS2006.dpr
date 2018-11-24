3515d5fb613001eb78072c4fcbdbeade166b5326
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2018-11-24 15:59:41 +0000

    added new unit GX_DetectIdeSearchPathForm
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@2454 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- Projects/Delphi2006/GExpertsBDS2006.dpr
+++ Projects/Delphi2006/GExpertsBDS2006.dpr
@@ -241,7 +241,8 @@ uses
   GX_IdeDockFormEnhancer in '..\..\Source\IDE\GX_IdeDockFormEnhancer.pas',
   GX_AddDockWindow in '..\..\Source\Experts\GX_AddDockWindow.pas' {fmGxDockForm},
   GX_eRemoveMatchingLines in '..\..\Source\Editor\GX_eRemoveMatchingLines.pas' {fmRemoveMatchingLinesExpertConfig},
-  GX_TabOrderOptions in '..\..\Source\TabOrder\GX_TabOrderOptions.pas' {fmTabOrderOptions};
+  GX_TabOrderOptions in '..\..\Source\TabOrder\GX_TabOrderOptions.pas' {fmTabOrderOptions},
+  GX_DetectIdeSearchPathForm in '..\..\Source\Framework\GX_DetectIdeSearchPathForm.pas';
 
 {$R '..\..\images\GXIcons.res' '..\..\images\GXIcons.rc'}
 {$R '..\..\Binaries\CodeProofreaderDefault.res' '..\..\Binaries\CodeProofreaderDefault.rc'}
