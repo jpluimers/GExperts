a63a6e3e5c3a81475be3c45bc44a2c4a58bc5c3e
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2018-12-02 13:11:18 +0000

    Copy of https://osdn.net/projects/dzlib-tools/svn/view/dzlib/trunk/src/u_dzCompilerAndRtlVersions.pas?view=markup&root=dzlib-tools (not yet used)
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@2505 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

---
+++ Source/Framework/GX_dzCompilerAndRtlVersions.pas
@@ -0,0 +1,70 @@
+unit GX_dzCompilerAndRtlVersions;
+
+interface
+
+const
+  CompilerVersionDelphi6 = 14;
+  CompilerVersionDelphi7 = 15;
+  CompilerVersionDelphi8 = 16;
+  // naming scheme changed to year numbers (curse you Microsoft!)
+  CompilerVersionDelphi2005 = 17;
+  CompilerVersionDelphi2006 = 18;
+  CompilerVersionDelphi2007 = 18.5;
+  CompilerVersionDelphi2007Net = 19;
+  CompilerVersionDelphi2009 = 20;
+  CompilerVersionDelphi2010 = 21;
+  // naming scheme changed to XEn
+  CompilerVersionDelphiXE = 22;
+  CompilerVersionDelphiXE2 = 23;
+  CompilerVersionDelphiXE3 = 24;
+  CompilerVersionDelphiXE4 = 25;
+  CompilerVersionDelphiXE5 = 26;
+  CompilerVersionDelphiXE6 = 27;
+  CompilerVersionDelphiXE7 = 28;
+  CompilerVersionDelphiXE8 = 29;
+  // Naming scheme changed to 10.n
+  CompilerVersionDelphiX100 = 30;
+  CompilerVersionDelphiX101 = 31;
+  CompilerVersionDelphiX102 = 32;
+  CompilerVersionDelphiX103 = 33;
+  // if you are better at memorizing city names
+  CompilerVersionDelphiSeattle = 30;
+  CompilerVersionDelphiBerlin = 31;
+  CompilerVersionDelphiTokyo = 32;
+  CompilerVersionDelphiRio = 33;
+
+const
+  RtlVersionDelphi6 = 14;
+  RtlVersionDelphi7 = 15;
+  RtlVersionDelphi8 = 16;
+  // naming scheme changed to year numbers (curse you Microsoft!)
+  RtlVersionDelphi2005 = 17;
+  RtlVersionDelphi2006 = 18;
+  RtlVersionDelphi2007 = 18.5;
+  RtlVersionDelphi2007Net = 19;
+  RtlVersionDelphi2009 = 20;
+  RtlVersionDelphi2010 = 21;
+  // naming scheme changed to XEn
+  RtlVersionDelphiXE = 22;
+  RtlVersionDelphiXE2 = 23;
+  RtlVersionDelphiXE3 = 24;
+  RtlVersionDelphiXE4 = 25;
+  RtlVersionDelphiXE5 = 26;
+  RtlVersionDelphiXE6 = 27;
+  RtlVersionDelphiXE7 = 28;
+  RtlVersionDelphiXE8 = 29;
+  // Naming scheme changed to 10.n
+  RtlVersionDelphiX100 = 30;
+  RtlVersionDelphiX101 = 31;
+  RtlVersionDelphiX102 = 32;
+  RtlVersionDelphiX103 = 33;
+  // if you are better at memorizing city names
+  RtlVersionDelphiSeattle = 30;
+  RtlVersionDelphiBerlin = 31;
+  RtlVersionDelphiTokyo = 32;
+  RtlVersionDelphiRio = 33;
+
+implementation
+
+end.
+
