a41532e6ebdc8e911706f03d741521d13461e02f
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2018-12-15 13:30:11 +0000

    global build script now increments global build number
    git-svn-id: https://svn.code.sf.net/p/gexperts/code/trunk@2535 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- Projects/__build.cmd
+++ Projects/__build.cmd
@@ -1,3 +1,8 @@
+@echo off
+call :FindInParents %0% buildtools
+"%result%\prepbuild.exe" --ReadIni=GExperts --IncBuild --updateIni=GExperts
+pause
+
 if "%1"=="" goto :list
 call :dobuild %1
 goto :eof
@@ -31,4 +36,34 @@ setlocal
 call __Build_Project.cmd
 endlocal
 popd
+goto :eof
+
+:FindInParents
+@rem search all parent directories for a subdir and return
+@rem the full path to that directory in %result%
+setlocal
+set parentdir=%1%
+set subdir=%2%
+:loop
+call :GetDir "%parentdir%"
+set parentdir=%result%
+if exist "%parentdir%\%subdir%" goto found
+goto loop
+:found
+endlocal & set result=%parentdir%\%subdir%
+goto :eof
+
+:GetDir
+rem extract path
+setlocal
+set result=%~dp1%
+rem remove any quotes
+set result=%result:"=%
+rem add quotes
+set result="%result%"
+rem remove \ before the closing quote
+set result=%result:\"="%
+rem remove any quotes
+set result=%result:"=%
+endlocal & set result=%result%
 goto :eof
